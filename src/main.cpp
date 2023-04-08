4d861db899d6e4853a3ad5ce8ab15df33c620a56
Abhinav Natarajan <abhinav.v.natarajan@gmail.com>
2023-04-08 17:14:18 +0100

    add arm64 for mac, remove musllinux

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@
 
 namespace py = pybind11;
 
-typedef CGAL::Epick_d<CGAL::Dynamic_dimension_tag> K;
-typedef K::Point_d Point;
-typedef std::vector<double> CartesianCoordinates;
+typedef CGAL::Epick_d<CGAL::Dynamic_dimension_tag> Kernel;
+typedef Kernel::Point_d Point;
+typedef std::vector<double> CoordinateVector;
+typedef CGAL::Delaunay_triangulation<Kernel> DelComplex;
 
 template <typename T>
 concept Iterable = requires(T x) {
@@ -28,14 +29,15 @@ int add(int i, int j) {
     return i + j;
 }
 
-// Convert a vector of 
-Point coords_to_point(const CartesianCoordinates &x) {
+// Convert a vector of coordinates to a CGAL point
+Point coordvec_to_point(const CoordinateVector &x) {
     return Point(x.begin(), x.end());
 }
 
+// Convert a collection of coordinate vectors to a vector of CGAL points
 template <typename Container_t>
-requires IterableOfType<Container_t, CartesianCoordinates>
-std::vector<Point> coords_to_point(const Container_t& x_arr) {
+requires IterableOfType<Container_t, CoordinateVector>
+std::vector<Point> coordvecs_to_points(const Container_t& x_arr) {
     std::vector<Point> points;
     for (auto x = x_arr.begin(); x != x_arr.end(); x++) {
         points.push_back(coords_to_point(*x));
@@ -43,24 +45,39 @@ std::vector<Point> coords_to_point(const Container_t& x_arr) {
     return points;
 }
 
-std::vector<double> point_to_coords(const Point &p) {
-    CartesianCoordinates x;
+// Convert a CGAL point to a coordinate vector
+std::vector<double> point_to_coordvec(const Point &p) {
+    CoordinateVector x;
     for(auto coord = p.cartesian_begin(); coord != p.cartesian_end(); coord++) {
         x.push_back(*coord);
     }
     return x;
 }
 
+// Convert a collection of CGAL points to a vector of coordinate vectors
 template <typename Container_t>
 requires IterableOfType<Container_t, Point>
-std::vector<CartesianCoordinates> point_to_coords(const Container_t& points) {
-    std::vector<CartesianCoordinates> x_arr;
+std::vector<CoordinateVector> points_to_coordvecs(const Container_t& points) {
+    std::vector<CoordinateVector> x_arr;
     for (auto p = points.begin(); p != points.end(); p++) {
         x_arr.push_back(point_to_coords(*p));
     }
     return x_arr;
 }
 
+// Create a Delaunay triangulation from a vector of coordinate vectors
+// template <typename Container_t>
+// requires IterableOfType<Container_t, CoordinateVector>
+// DelComplex del_complex(const Container_t& x_arr) {
+//     if (x_arr.size() == 0) {
+//         return 
+//     }
+//     for (auto it = x_arr.begin(); it != x_arr.end(); it++) {
+//         if it->size
+//     }
+//     auto points = coordvecs_to_points(x_arr);
+    
+// }
 
 PYBIND11_MODULE(_core, m) {
     m.doc() = R"pbdoc(
