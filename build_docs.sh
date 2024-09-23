a72b130181f28919bd468d2433bf4245d0512976
Abhinav Natarajan <abhinav.v.natarajan@gmail.com>
2024-09-23 17:13:11 +0100

    (docs) build docs with Python 3.12

--- build_docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -m pybind11_stubgen chalc.chromatic --numpy-array-use-type-var --output-dir src
-python -m pybind11_stubgen chalc.filtration --numpy-array-use-type-var --output-dir src
-sphinx-build -E -b html docs docs/_build
