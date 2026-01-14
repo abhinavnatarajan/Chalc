b1d4ce1ea6e86fc24b0ba909c1a25c67f1b29903
Abhinav Natarajan <abhinav.v.natarajan@gmail.com>
2026-01-14 17:32:23 +0000

    chore: remove license text from source files

--- src/chalc/filtration/filtration.h
+++ src/chalc/filtration/filtration.h
@@ -1,38 +1,3 @@
-/*
-This file is part of Chalc.
-
-Chalc: Chromatic Alpha Complexes.
-Based on: di Montesano et. al., “Persistent Homology of Chromatic Alpha
-Complexes”. Online preprint available at http://arxiv.org/abs/2212.03128.
-Accessed: 2023-02-28 22:07:23 UTC.
-DOI: 10.48550/arXiv.2212.03128.
-
-Project homepage:    http://github.com/abhinavnatarajan/Chalc
-
-Copyright (c) 2023 Abhinav Natarajan
-
-Contributors:
-Abhinav Natarajan
-
-Licensing:
-Chalc is released under the GNU General Public License ("GPL").
-
-GNU General Public License ("GPL") copyright permissions statement:
-**************************************************************************
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 #ifndef FILTRATION_H
 	#define FILTRATION_H
